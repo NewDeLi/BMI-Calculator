afd277b92d15987c2fdba67a25a6b87fff38e466
NewDeLi <lindnerdenis@web.de>
2021-12-28 17:09:49 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'last test',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
