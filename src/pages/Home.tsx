7cfda4fe6d12a9b210324d4495e2dac35b5cd9de
NewDeLi <lindnerdenis@web.de>
2021-12-28 18:49:20 +0100

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,8 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonPage } from "@ionic/react";
+import "./Home.css";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+  return <IonPage></IonPage>;
 };
 
 export default Home;
